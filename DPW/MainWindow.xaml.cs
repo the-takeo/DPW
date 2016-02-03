0a1a173793bd8b3cce6983df3c34a67a1312c5db
TAKEO <mr.thetakeo@gmail.com>
2016-02-03 23:55:18 +0800

    use WPF

---
+++ DPW/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DPW
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Model model { get; set; }
+        System.ComponentModel.BackgroundWorker bgWorker;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            model = new Model();
+            this.DataContext = model;
+        }
+
+        private void btnFolder_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                model.Folder = fbd.SelectedPath;
+            }
+        }
+
+        private void btnDownload_Click(object sender, RoutedEventArgs e)
+        {
+            startProgress();
+            model.getUrls();
+
+            bgWorker = new System.ComponentModel.BackgroundWorker();
+            bgWorker.WorkerReportsProgress = true;
+            bgWorker.DoWork += bgWorker_DoWork;
+            bgWorker.ProgressChanged += bgWorker_ProgressChanged;
+            bgWorker.RunWorkerCompleted += bgWorker_RunWorkerCompleted;
+
+            bgWorker.RunWorkerAsync();
+        }
+
+        void bgWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("ダウンロードが完了しました。");
+
+            model.SaveSetting();
+            refleshView();
+            endProgress();
+        }
+
+        void bgWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            pb.Value = 100 * model.count / model.Urls.Count;
+        }
+
+        void bgWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            Download dl = new Download();
+
+            //allPictureNum = model.Urls.Count;
+
+            foreach (var url in model.Urls)
+            {
+                try { dl.StartDownload(url, model.Folder); }
+                finally
+                {
+                    //バインドを検討
+                    model.count++;
+                    bgWorker.ReportProgress(model.count);
+                }
+            }
+        }
+
+        private void refleshView()
+        {
+            model.refleshUrls();
+            pb.Value = 0;
+        }
+
+        private void startProgress()
+        {
+            tbUrl.IsEnabled = false;
+            tbFolder.IsEnabled = false;
+            btnFolder.IsEnabled = false;
+            btnDownload.IsEnabled = false;
+        }
+
+        private void endProgress()
+        {
+            tbUrl.IsEnabled = true;
+            tbFolder.IsEnabled = true;
+            btnFolder.IsEnabled = true;
+            btnDownload.IsEnabled = true;
+        }
+
+
+    }
+}
---
+++ DPW/Model.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPW
+{
+    public class Model : System.ComponentModel.INotifyPropertyChanged
+    {
+        #region WPFデータバインド用
+
+        public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new System.ComponentModel.PropertyChangedEventArgs(name));
+            }
+        }
+
+        #endregion
+
+        private string url_;
+        private string folder_;
+        private List<string> urls_;
+
+        public string Url
+        {
+            get
+            {
+                return url_;
+            }
+            set
+            {
+                url_ = value;
+                OnPropertyChanged("Url");
+            }
+        }
+        public string Folder
+        {
+            get
+            {
+                return folder_;
+            }
+            set
+            {
+                folder_ = value;
+                OnPropertyChanged("Folder");
+            }
+        }
+
+        public List<string> Urls
+        {
+            get 
+            {
+                if (urls_ == null)
+                { return GetPictures.getPicturesUrl(Url, null); }
+                else
+                { return urls_; }
+            }
+        }
+
+        public int count { get; set; }
+
+        public Model()
+        { LoadSetting(); }
+
+        public void LoadSetting()
+        {
+            Url = Properties.Settings.Default.Url;
+            Folder = Properties.Settings.Default.Folder;
+        }
+
+        public void SaveSetting()
+        {
+            Properties.Settings.Default.Url = Url;
+            Properties.Settings.Default.Folder = Folder;
+
+            Properties.Settings.Default.Save();
+        }
+
+        public void refleshUrls()
+        { urls_ = null; }
+
+        public void getUrls()
+        {
+            urls_ = GetPictures.getPicturesUrl(Url, null);
+        }
+    }
+}
